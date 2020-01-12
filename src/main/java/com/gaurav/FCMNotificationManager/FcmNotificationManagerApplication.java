d156e8c4e5cd36789a296ced56a8150d76411778
Gaurav Sharma <dev.gaurav94@gmail.com>
2020-01-12 20:28:30 +0530

    init

---
+++ src/main/java/com/gaurav/FCMNotificationManager/FcmNotificationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.FCMNotificationManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmNotificationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcmNotificationManagerApplication.class, args);
+	}
+
+}
