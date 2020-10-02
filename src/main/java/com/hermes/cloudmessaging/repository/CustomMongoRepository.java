0ff9c46aeb88fced112099d6812fec10d7dd8085
gaurav <dev.gaurav94@gmail.com>
2020-10-02 19:53:01 +0530

    added code for creating index on the fly

--- src/main/java/com/hermes/cloudmessaging/repository/CustomMongoRepository.java
+++ src/main/java/com/hermes/cloudmessaging/repository/CustomMongoRepository.java
@@ -1,10 +1,13 @@
 package com.hermes.cloudmessaging.repository;
 
+import com.hermes.cloudmessaging.dto.request.IndexCreationRequest;
 import com.hermes.cloudmessaging.entity.mongo.CloudMessagingRegistryEntity;
 
 import java.util.List;
 
-public interface CustomMongoRepository {
+public interface CustomMongoRepository<T> {
 
     List<CloudMessagingRegistryEntity> query(Object o, String strQuery);
+
+    void createIndexes(Class<CloudMessagingRegistryEntity> clazz, IndexCreationRequest indexCreationRequest);
 }
