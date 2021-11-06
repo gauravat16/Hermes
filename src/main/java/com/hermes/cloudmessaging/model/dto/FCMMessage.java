87368dcaf0776ee187ad6fa4d7564e12fc371515
gaurav <dev.gaurav94@gmail.com>
2021-11-06 20:20:51 +0530

    code refactored

--- src/main/java/com/hermes/cloudmessaging/model/dto/FCMMessage.java
+++ src/main/java/com/hermes/cloudmessaging/model/dto/FCMMessage.java
@@ -12,20 +12,20 @@ import java.util.Map;
 @NoArgsConstructor
 public class FCMMessage {
 
-    private Map<String, String> data;
+    private Map<String, Object> data;
 
     private String to;
 
     @JsonAlias("collapse_key")
     private String collapseKey;
 
-    public FCMMessage(Map<String, String> data, String to, String collapseKey) {
+    public FCMMessage(Map<String, Object> data, String to, String collapseKey) {
         this.data = data;
         this.to = to;
         this.collapseKey = collapseKey;
     }
 
-    public static FCMMessage of(Map<String, String> data, String to, String collapseKey) {
+    public static FCMMessage of(Map<String, Object> data, String to, String collapseKey) {
         return new FCMMessage(data, to, collapseKey);
     }
 
